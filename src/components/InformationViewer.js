86f4cc1cddfd3e3ecac7b4893a621bb7c144dce6
Andrew <andrew.karnis@gmail.com>
2021-04-30 13:14:11 -0400

    display data

--- src/components/InformationViewer.js
+++ src/components/InformationViewer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 function InformationViewer(props) {
     const [gridApi, setGridApi] = useState(null);
@@ -16,13 +16,16 @@ function InformationViewer(props) {
         setGridColumnApi(params.columnApi);
     };
     return (
-        <div class="flex flex-1 info-viewer">
-            <AgGridReact
-                onGridReady={onGridReady}
-                rowData={rowData}>
-                <AgGridColumn headerName="Athlete" field="athlete" />
-                <AgGridColumn headerName="Sport" field="sport" />
-            </AgGridReact>
+        <div style={{ width: '100%', height: '100%' }}>
+            <div className="ag-theme-balham" style={{ height: '100%', boxSizing: 'border-box' }}>
+                <AgGridReact
+                    style={{ width: '100%', height: '100%;' }}
+                    onGridReady={onGridReady}
+                    rowData={rowData}>
+                    <AgGridColumn headerName="Athlete" field="athlete" />
+                    <AgGridColumn headerName="Sport" field="sport" />
+                </AgGridReact>
+            </div>
         </div>
     );
 }
