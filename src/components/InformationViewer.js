febcb212c747a796e41f6ed7910d117fbe07f651
gammaseeker <joeyjiem@gmail.com>
2021-04-30 16:04:04 -0400

    moved getAllData to schools component

--- src/components/InformationViewer.js
+++ src/components/InformationViewer.js
@@ -10,12 +10,8 @@ function InformationViewer(props) {
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
     const [rowData, setRowData] = useState(null);
-    let pageCtr = 0;
-    let collegeData = [];
-    let hasResults = true;
-    useEffect(async () => {
-        await getAllData();
-        setRowData(collegeData);
+    useEffect(() => {
+        setRowData(props.rowData);
     }, []);
 
     const onGridReady = (params) => {
@@ -23,31 +19,6 @@ function InformationViewer(props) {
         setGridColumnApi(params.columnApi);
     };
 
-    const getAllData = async () => {
-        while(hasResults){
-            if(pageCtr === 0){
-                const url = `https://api.data.gov/ed/collegescorecard/v1/schools?api_key=${apiKey}`;
-                await fetch(url).then((resp) => resp.json()).then((data) => {
-                    console.log(data);
-                    collegeData = collegeData.concat(data.results);
-                    console.log(collegeData);
-                    pageCtr++;
-                });
-            } else {
-                const url = `https://api.data.gov/ed/collegescorecard/v1/schools?api_key=${apiKey}&page=${pageCtr}&per_page=100`;
-                await fetch(url).then((resp) => resp.json()).then((data) => {
-                    if(data.results.length === 0 || data.results === undefined) {
-                        hasResults = false;
-                    } else {
-                        console.log(data);
-                        collegeData = collegeData.concat(data.results);
-                        console.log(collegeData);
-                        pageCtr++;
-                    }
-                });
-            }
-        }
-    }
     return (
         <div style={{ width: '100%', height: '100%' }}>
             <div className="ag-theme-balham" style={{ height: '100%', boxSizing: 'border-box' }}>
