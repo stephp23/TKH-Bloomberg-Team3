9af44e8ad4fc8dcb9cbb4d91f1385f0fa3ef82cd
Andrew <andrew.karnis@gmail.com>
2021-04-30 17:15:33 -0400

    remove log

--- src/components/InformationViewer.js
+++ src/components/InformationViewer.js
@@ -5,7 +5,6 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 function InformationViewer(props) {
-    console.log(props);
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
     const [rowData, setRowData] = useState(null);
