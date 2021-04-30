5fc45b830a65f6693cc11ac8f8fa13a750cc0c30
Joey Jiemjitpolchai <joeyjiem@gmail.com>
2021-04-30 10:40:55 -0400

    added bootstrap (#1)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
