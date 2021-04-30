9a840dbe7d94a231aac4877f9f515a956b2b1eb8
Andrew <andrew.karnis@gmail.com>
2021-04-30 11:59:11 -0400

    Info viewer

---
+++ src/components/InformationViewer.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function InformationViewer(props) {
+    return (
+        <div class="flex-column flex-1 info-viewer">
+
+        </div>
+        );
+}
+
+export default InformationViewer;
