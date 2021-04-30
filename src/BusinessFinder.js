16366ad8042dc1a84d9b197cda38e5fb5bb36fc5
Andrew <andrew.karnis@gmail.com>
2021-04-30 11:34:04 -0400

    update layout

--- src/BusinessFinder.js
+++ src/BusinessFinder.js
@@ -1,7 +1,17 @@
-<div class="flex-column">
-    <div class="flex-column">
-        <div class="flex-1"></div>
-        <div class="flex-4"></div>
-    </div>
-    <div class="flex footer"></div>
-</div>
+import React, { useState } from 'react';
+
+function BusinessFinder(props) {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div class="flex-column">
+            <div class="flex-column">
+                <div class="flex-1 sidebar"></div>
+                <div class="flex-4"></div>
+            </div>
+            <div class="flex footer"></div>
+        </div>
+        );
+}
+
+export default BusinessFinder;
