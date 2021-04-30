71d7707b4d24cad804d93fc115f72600e425dc1a
Andrew <andrew.karnis@gmail.com>
2021-04-30 17:08:03 -0400

    remove unused variables

--- src/components/CollegeFinder.js
+++ src/components/CollegeFinder.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import InformationViewer from './InformationViewer';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Schools from './Schools';
 
