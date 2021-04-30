72bf513af471177168b5ab996591c35c30a60b2b
Andrew <andrew.karnis@gmail.com>
2021-04-30 11:17:17 -0400

    layout

---
+++ src/BusinessFinder.js
@@ -0,0 +1,7 @@
+<div class="flex-column">
+    <div class="flex-column">
+        <div class="flex-1"></div>
+        <div class="flex-4"></div>
+    </div>
+    <div class="flex footer"></div>
+</div>
