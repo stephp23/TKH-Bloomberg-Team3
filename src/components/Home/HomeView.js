77639bc360c25f238fe382cc8ef86f899016735e
Andrew <andrew.karnis@gmail.com>
2021-05-01 10:58:28 -0400

    latest

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import Footer from './components/Footer/Footer';
 import Home from '../components/Home';
-import Sidebar from './Sidebar';
 
 
 
@@ -20,11 +18,6 @@ const HomeView = () => {
       </div>
     </div>
   )
-  // return (
-  //   <div>
-  //     <h1>This is home</h1>
-  //   </div>
-  // )
 }
 
 export default HomeView
