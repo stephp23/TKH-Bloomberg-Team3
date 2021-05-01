b565502092e0577386f874e86bde09f7a324f717
stephp23 <stephp23@gmail.com>
2021-05-01 13:28:00 -0400

    header navigation tabs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,96 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import './Header.css';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 // import logo from '...src/images/logo.png';
 
-const Header = () => {
+    
+    // <div className="flex flex">
+    //   <Button variant="contained">Home</Button>
+    //   <Button variant="contained">Schools</Button>
+    //   <Button variant="contained">Degrees</Button>
+    // </div>
+
+
+// export default Header;
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="flex flex">
-      <Button variant="contained">Home</Button>
-      <Button variant="contained">Schools</Button>
-      <Button variant="contained">Degrees</Button>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
-  )
+  );
 }
 
-export default Header;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Home" {...a11yProps(0)} />
+          <Tab label="Schools" {...a11yProps(1)} />
+          <Tab label="Degree Search" {...a11yProps(2)} />
+          <Tab label="Financials" {...a11yProps(3)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Home
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Schools
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Degree Search
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Financials
+      </TabPanel>
+    </div>
+  );
+}
---
+++ src/components/Reports/Financials/Financials.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Financials = () => {
+  return (
+    <div>
+      <h1>Search by Degree Types</h1>
+    </div>
+  )
+}
+
+export default Financials
