53dc35ba8ae49e13728b5fc20947ef27288ed947
Andrew <andrew.karnis@gmail.com>
2021-05-01 13:28:54 -0400

    initial data grouping

--- src/components/Reports/DegreeSearch.js
+++ src/components/Reports/DegreeSearch.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DetailViewer from '../DetailViewer.js';
 
 
 function DegreeSearch(props) {
-  console.log(props)
+  const columns = getColumns();
+  const [data, setData] = useState(null);
+  const [columnDefs, setColumnDefs] = useState(columns);
+  useEffect(() => {
+    const groupedData = getGroupedData(props.data);
+    setData(groupedData);
+  }, [props.data]);
   return (
-      <div className="flex-column">
-        <DetailViewer></DetailViewer>
-      </div>
+    <div className="flex-column">
+      <DetailViewer rowData={data} columnDefs={columnDefs}></DetailViewer>
+    </div>
   );
+}
+
+function getColumns() {
+  return [
+    { headerName: "Major", field: "title", width: 500}
+  ];
+}
+
+function getGroupedData(data) {
+  const grouped = data && data.reduce((acc, cur, idx) => {
+    if (cur.latest && cur.latest.programs) {
+      for (let major of cur.latest.programs.cip_4_digit) {
+        if (!acc.hasOwnProperty(major.code)) {
+          acc[major.code] = {
+            code: major.code,
+            title: major.title,
+            schools: []
+          }
+        }
+        acc[major.code].schools.push(cur.school);
+      }
+    }
+    return acc;
+  }, {});
+  if (grouped) {
+    return Object.keys(grouped).map((majorCode) => {
+      return grouped[majorCode];
+    });
+  }
+  return [];
 }
 
 export default DegreeSearch
