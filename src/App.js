38c010fc1fa65fc8f2bb4ce0dc4380b7d2189980
Andrew <andrew.karnis@gmail.com>
2021-05-01 10:48:39 -0400

    update component structure

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import './App.css';
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar';
 import Home from './components/Home/Home';
-import InformationViewer from '../src/components/InformationViewer';
-// import Schools from '../src/components/Schools';
-import CollegeFinder from './components/Locations/CollegeFinder';
-import DegreeSearch from './components/Degrees/DegreeSearch';
+import Schools from '../src/components/Reports/Schools';
+import DegreeSearch from './components/Reports/Degrees/DegreeSearch';
 import Footer from './components/Footer/Footer';
 
 
 
 function App() {
   return (
-    <div className="flex-column flex-1 pad-10"> 
-       <Header />
-       <Switch>
-        <Route path="/" exact component={Home} />
-         <Route path="/location" exact component={CollegeFinder} />
-         <Route path="/degree" exact component={DegreeSearch} />
-         <Route path="/allinfo" exact component={InformationViewer} />
-       </Switch>
-       <Footer />  
-       </div>
+    <div className="flex-column flex-1 pad-10">
+      <Header />
+      <div class="flex-column flex-1">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/schools" exact component={Schools} />
+          <Route path="/degree" exact component={DegreeSearch} />
+        </Switch>
+      </div>
+      <div class="flex footer-div">
+        <Footer />
+      </div>
+    </div>
   );
 }
 
